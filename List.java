7ce7aab438f05616cb5f0bb94f2a63389605ff1c
Kyle-29-L <kylelabatete29@gmail.com>
2019-10-03 09:35:44 -0600

    started ass2

---
+++ List.java
@@ -0,0 +1,10 @@
+class List
+{
+	public static void main (String[] args)
+	{
+		ListElement le = new ListElement();
+		le.setData(5);
+		le.addElement
+		
+	}
+}
---
+++ ListElement.java
@@ -0,0 +1,39 @@
+public class ListElement
+{
+	private ListElement *next;
+	private int data;
+	
+	public ListElement()
+	{
+		this.data = 0;
+		this.next = null;
+	}
+	
+	public void setData(int data)
+	{
+		this.data = data;
+	}
+	
+	public int getData()
+	{
+		return this.data;
+	}
+	public void addElement()
+	{	
+	
+	}
+	public ListElement getElement (int index)
+	{
+		
+	}
+	public ListElement deleteElement (int index)
+	{
+		
+	}
+	public void printLinkedListHead()
+	{
+		
+	}
+	
+}
+	
